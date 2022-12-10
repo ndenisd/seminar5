e82af956a38048fd8eff2e0fa0e70df42d646cd1
denis <ndenisd@yandex.ru>
2022-12-10 16:08:50 +0300

    homework

---
+++ Task34/Program.cs
@@ -0,0 +1,28 @@
+﻿// task 34
+
+void InputArray(int[] array)
+{
+for (int i = 0; i < array.Length; i++)
+array[i] = new Random().Next(100, 1000); // [100, 999]
+}
+
+
+int ReleaseArray(int[] array)
+{
+int countEven = 0;
+for (int i = 0; i < array.Length; i++)
+{
+if (array[i] % 2 == 0)
+countEven++;
+}
+return countEven;
+}
+
+
+Console.Clear();
+Console.Write("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+Console.WriteLine($"Результат: {ReleaseArray(array)}");
---
+++ Task36/Program.cs
@@ -0,0 +1,30 @@
+﻿// task 36
+
+void InputArray(int[] array)
+{
+for (int i = 0; i < array.Length; i++)
+{
+int x = new Random().Next(100, 1000); // [100, 999]
+while (array.Contains(x))
+x = new Random().Next(100, 1000); // [100, 999]
+array[i] = x;
+}
+}
+
+
+int ReleaseArray(int[] array)
+{
+int sumUnEven = 0;
+for (int i = 1; i < array.Length; i+=2)
+sumUnEven += array[i];
+return sumUnEven;
+}
+
+
+Console.Clear();
+Console.Write("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+Console.WriteLine($"Результат: {ReleaseArray(array)}");
